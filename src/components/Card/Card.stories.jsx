765263d048d2f074a55fac12dce9999b0f328e26
Ruslan Useinov <r.useinov23@gmail.com>
2022-08-01 11:24:09 -0400

    Card story content updated

--- src/components/Card/Card.stories.jsx
+++ src/components/Card/Card.stories.jsx
@@ -4,10 +4,8 @@ import React from 'react'
 import Card from './Card'
 import algovis from './../../assets/algovisGif.gif'
 
-import { SiTypescript } from "react-icons/si";
-import { SiReact } from "react-icons/si";
+import { SiTypescript, SiReact } from "react-icons/si";
 import { FaGithub } from 'react-icons/fa';
-
 
 export default{
     title:'Card ',
@@ -21,8 +19,11 @@ export const DesktopCard = Template.bind({});
 DesktopCard.args = {
     heading:'Heading',
     subHeading:'SubHeading',
-    text:'Tellus elementum sagittis vitae et leo duis ut diam quam nulla porttitor massa id neque aliquam vestibulum morbi blandit cursus risus at ultrices mi tempus imperdiet nulla malesuada pellentesque elit eget gravida cum sociis natoque penatibus et magnis dis parturient montes nascetur ridiculus mus mauris',
+    text:[
+        'Tellus elementum sagittis vitae et leo duis ut diam quam nulla porttitor massa id neque aliquam vestibulum morbi blandit cursus risus at ultrices mi tempus imperdiet nulla malesuada pellentesque elit eget.', 
+        'Gravida cum sociis natoque penatibus et magnis dis parturient montes nascetur ridiculus mus mauris'
+    ],
     img:algovis,
-    tech:[[<SiReact/>, 'React'], [ <SiTypescript/>, 'Typescript'], 'D3.JS'],
-    links:[ {link:'https://github.com/ruslaaaan/algo_visualization', label:[<FaGithub/>, 'GitHub']} ]
+    tech:[[<SiReact/>, 'Сhip1'], [ <SiTypescript/>, 'Сhip2'], 'Сhip3'],
+    links:[ {link:'https://github.com', label:[<FaGithub/>, 'Github']},{link:'https://google.com', label:[ 'Website.com']} ]
 }
