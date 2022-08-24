db5112a164b37c49ebade731f8d4281311eb7c48
Ruslan Useinov <r.useinov23@gmail.com>
2022-08-24 15:14:40 -0400

    About page texta dded, styles changed

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,6 +2,7 @@ import React, { useContext, useEffect } from 'react'
 import gsap from 'gsap'
 import { TextPlugin } from "gsap/TextPlugin.js";
 import {LayoutContext} from '@components/Layout/Context/LayoutContextProvider'
+import LinkCustom from '@components/atoms/LinkCustom/LinkCustom'
 
 import 'styles/pages/about.scss'
 
@@ -18,22 +19,28 @@ const About = () => {
     useEffect( ()=>{
         const mainTimeline = gsap.timeline()
 
-        const textTl1 = 'Hi, my name is Ruslan and I love to build the Web.';
-        const textTl3 = 'I’m here to ';
+        //texta assets
+        const textTl1 = 'Hi :)';
+        const textTl2 = 'I am Ruslan Useinov.';
+        const textTl3 = 'And I love to build the Web!';
+        const textTl4 = 'I’m here to';
 
-        const textTl4 = 'Build ';
-        const textTl5 = 'Learn ';
-        const textTl6 = 'Grow ';
-        const textTl7 = 'Make friends';
-        const textTl8 = 'And help others do the same';
+        const textTl5 = 'Build';
+        const textTl6 = 'Learn';
+        const textTl7 = 'Grow';
+        const textTl8 = 'Make friends';
+        const textTl9 = 'And help others do the same';
 
-        const listTl1 = [ 'open-minded.',              'analytical.',                  'a good listener.'     ]
-        const listTl2 = [ 'collaborative work.',       'challenge.',                   'constantly improving.']
-        const listTl3 = [ 'honesty.',                  'quality.',                     'comradery.'           ]
-        const listTl4 = [ 'being positive-oriented.',  'being receptive to feedback.', 'to have a goal.'      ]
+        const listTl1 = [ 'open-minded ',             'analytical',                  'a good listener'      ];
+        const listTl2 = [ 'collaborative work',       'challenge',                   'constantly improving' ];
+        const listTl3 = [ 'honesty',                  'quality',                     'comradery'            ];
+        const listTl4 = [ 'being positive-oriented',  'being receptive to feedback', 'to have a goal'       ];
 
-        //timelines queue:
-        mainTimeline.to('#box-text-1', { duration: 1.5, delay:1.5, text: textTl1, })
+        //timelines queue in top down order:
+        mainTimeline.to('#box-text-1', { duration: 1, delay:1.4, text: textTl1, })
+        mainTimeline.to('#box-text-2', { duration: 1, delay:.8, text: textTl2, })
+        mainTimeline.to('#box-text-3', { duration: 1, delay:.8, text: textTl3, })
+        
         mainTimeline.from(".start-phrase",  {
             y: 100,
             ease: "power4.out",
@@ -45,7 +52,7 @@ const About = () => {
             }
         })
 
-        function verticalTimeline(){
+        function verticalOrderTimeline(){
             const matrix = [listTl1, listTl2, listTl3, listTl4]
     
             for(let i = 0; i<matrix[0].length; i++){
@@ -58,22 +65,25 @@ const About = () => {
                 }
             }
         }
-        verticalTimeline()
-        mainTimeline.to('#box-text-3', { duration: 2, delay:1, text: textTl3, })
-
-        mainTimeline.to('#box-text-4', { duration: .7, delay:.2, text: textTl4, })
-        mainTimeline.to('#box-text-5', { duration: .7, delay:.2, text: textTl5, })
-        mainTimeline.to('#box-text-6', { duration: .7, delay:.2, text: textTl6, })
-        mainTimeline.to('#box-text-7', { duration: .7, delay:.2, text: textTl7, })
-        mainTimeline.to('#box-text-8', { duration: 2, delay:.5, text: textTl8, })
+        verticalOrderTimeline()
 
 
+        mainTimeline.to('#box-text-4', { duration: 1.3, delay:1, text: textTl4, })
+        mainTimeline.to('#box-text-5', { duration: .5, delay:.2, text: textTl5 })
+        mainTimeline.to('#box-text-6', { duration: .5, delay:.2, text: textTl6 })
+        mainTimeline.to('#box-text-7', { duration: .5, delay:.2, text: textTl7 })
+        mainTimeline.to('#box-text-8', { duration: 1.5, delay:.5, text: textTl8 })
+        mainTimeline.to('#box-text-9', { duration: 1.5, delay:.5, text: textTl9 })
+        mainTimeline.to('#link-to-resume', { duration: 1.5, delay:.5, opacity:1 })
     }, [])
 
     return (
         <div className={'about-me-page'}>
 
             <span className='box-type-1' id='box-text-1'></span>
+            <span className='box-type-1' id='box-text-2'></span>
+            <span className='box-type-1' id='box-text-3'></span>
+            
 
             <div className='text-block'>
                 <div className='static-line-container'>
@@ -99,21 +109,29 @@ const About = () => {
 
             <div className='text-block'>
                 <div className='static-line-container'>
-                    <div className='start-phrase'>I think the key to growing and striving is
+                    <div className='start-phrase'>I think the key to grow is
                         <span className='box-type-2' id='box-list-3'></span>
                     </div>
                 </div>
             </div>
             
-            <span className='box-type-1' id='box-text-2'></span>
-            <span className='box-type-1' id='box-text-3'></span>
+            <span className='box-type-1' id='box-text-4'></span>
+            
+  
 
-            <div clasName='text-group'>
-                <span className='box-type-2' id='box-text-4'></span>
+            <div clasName='text-group' style={{marginTop:'2rem'}}>
+                <span className='box-type-2' id='box-text-5'></span>
                 <span className='box-type-2' id='box-text-6'></span>
                 <span className='box-type-2' id='box-text-7'></span>
+                <span className='box-type-2' id='box-text-8'></span>
             </div>
-            <span className='box-type-2 highlight final' id='box-text-8'></span>
+            <span className='box-type-2 highlight final' id='box-text-9'></span>
+
+            <div style={{opacity:0, marginTop:'2rem'}} id='link-to-resume'>
+                <LinkCustom medium  link={'https://www.linkedin.com/in/ruslan-useinov-330b5a23a/'} >
+                    Resume
+                </LinkCustom>
+            </div>
         </div>
     )
 }
