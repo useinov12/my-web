281652fc69eb67c1342044d963fdeba025ac53ec
Ruslan Useinov <r.useinov23@gmail.com>
2022-08-31 16:16:28 -0400

    Project page animation added

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,23 +1,36 @@
-import React, {useContext, useEffect} from 'react'
-
+import React, {useContext, useEffect, useRef} from 'react'
+import gsap from 'gsap'
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 import {LayoutContext} from '@components/Layout/Context/LayoutContextProvider'
 import projects from '@assets/projects/projects'
 import Card from '@components/Card/Card'
 
 const Projects = () => {
+    gsap.registerPlugin(ScrollTrigger);
+    ScrollTrigger.refresh();
     const {openMenu, setOpenMenu} = useContext(LayoutContext);
 
+    const porjectListRef = useRef([]);
 
     useEffect(() => {
         if(!openMenu)setOpenMenu(true)
     }, [])
 
+    useEffect(()=>{
+
+        const timeline = gsap.timeline()
+        gsap.utils.toArray(porjectListRef.current.children).forEach( card => {
+            timeline.from(card, {
+                opacity:0, 
+                duration:.7
+            })
+        })
+    },[])
+
     return (
-        <>
-            {
-                projects.map( projectArgs => <Card key={projectArgs.heading} {...projectArgs} />)
-            }
-        </>
+        <div className='projects-page' ref={porjectListRef}>
+            { projects.map( projectArgs =>  <Card key={projectArgs.id} {...projectArgs} /> )}
+        </div>
     )
 }
 
